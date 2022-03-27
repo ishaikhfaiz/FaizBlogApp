d3a03b664204ce3cd1e8caabd6d48d135aef4d81
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:57:15 +0530

    same

--- server/database.js
+++ server/database.js
@@ -1,12 +1,30 @@
-const Client = require('pg')
+const { Pool } = require('pg');
 
-const client = new Client({
-  host: 'ec2-52-201-124-168.compute-1.amazonaws.com',
-  user: 'znbfzhztgogqjp',
-  port: '5432',
-  password: '05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7',
-  database: 'dakv864hep6lie'
+const pool = new Pool();
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
-client.connect();
-client.end();
+pool.on('connect', (err, client) => {
+  if (err) console.error(err);
+  console.log(client);
+  console.log('Successfully connected to postgres.');
+});
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABSE_URL,{
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  dialectOptions: {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+    }
+  },
+});
+
+module.exports = sequelize;
