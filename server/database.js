882ec6de8dd516da4d711c0cd01806e96f22cda6
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 17:22:29 +0530

    new

--- server/database.js
+++ server/database.js
@@ -1,27 +1,22 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
-
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'clinica-veterinaria',
+  password: 'admin',
+  port: 5432,
 });
 
-pool.on('connect', (err, client) => {
-  if (err) console.error(err);
-  console.log(client);
-  console.log('Successfully connected to postgres.');
-});
-
-const Sequelize = require('sequelize');
-
-const connectionString = process.env.DATABASE_URL;
-const sequelize = new Sequelize(connectionString, {
-  logging: false,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true,
-    rejectUnauthorized: false
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
   }
-});
-module.exports = sequelize;
+  client.query('SELECT NOW()', (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(result.rows)
+  })
+})
