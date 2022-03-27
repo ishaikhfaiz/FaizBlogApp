146c240dd5dfa2f3625b65d9b35a79acf08dfdb8
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 19:33:05 +0530

    3

--- server/database.js
+++ server/database.js
@@ -1,18 +1,12 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'znbfzhztgogqjp',
-  host: 'ec2-52-201-124-168.compute-1.amazonaws.com',
-  database: 'dakv864hep6lie',
-  password: '05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7',
-  port: 5432,
-});
-
-pool.connect((err, client, release) => {
+const { Client } = require('pg')
+const client = new Client()
+client.connect(err => {
   if (err) {
-    return console.error('Error acquiring client', err.stack)
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
   }
-});
+})
 
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
