b2a04807b4203f6fde4b1569da28ed5d3d4f0106
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 15:01:47 +0530

    conn

--- server/database.js
+++ server/database.js
@@ -1,17 +1,20 @@
-const { Pool, Client } = require('pg')
-const connectionString = 'postgres://znbfzhztgogqjp:05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dakv864hep6lie'
-const pool = new Pool({
-  connectionString,
-})
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-const client = new Client({
-  connectionString,
-})
-client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+const { Pool } = require('pg');
+
+const pool = new Pool({connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+pool.on('connect', (err, client) => {
+  if (err) console.error(err);
+  console.log(client);
+  console.log('Successfully connected to postgres.');
+});
+
+
