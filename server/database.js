574d321f6521d09b654de81606867e4669c86df5
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:54:10 +0530

    cleint

--- server/database.js
+++ server/database.js
@@ -1,12 +1,11 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://znbfzhztgogqjp:05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dakv864hep6lie?sslmode=require', {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-  }
-});
-module.exports = sequelize;
+const Client = require('pg')
+
+const client = new Client({
+  host: 'ec2-52-201-124-168.compute-1.amazonaws.com',
+  user: 'znbfzhztgogqjp',
+  port: '5432',
+  password: '05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7',
+  database: 'dakv864hep6lie'
+}) 
+
+client.connect();
