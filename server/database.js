7cf701c1b429b31e1a385bea8f5e691c2865b9bc
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 19:23:30 +0530

    1

--- server/database.js
+++ server/database.js
@@ -12,13 +12,6 @@ pool.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack)
   }
-  client.query('SELECT NOW()', (err, result) => {
-    release()
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows)
-  })
 });
 
 const { Sequelize } = require('sequelize');
