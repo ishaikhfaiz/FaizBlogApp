3a2a8fabfb3c4e4bb5923a30dd0d121f4ff01287
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:01:59 +0530

    pool

--- server/database.js
+++ server/database.js
@@ -1,28 +1,40 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require("pg");
 
-const pool = new Pool();
+const credentials = {
+  user: "znbfzhztgogqjp",
+  host: "ec2-52-201-124-168.compute-1.amazonaws.com",
+  database: "dakv864hep6lie",
+  password: "05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7",
+  port: 5432,
+};
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+// Connect with a connection pool.
 
-pool.on('connect', (err, client) => {
-  if (err) console.error(err);
-  console.log(client);
-  console.log('Successfully connected to postgres.');
-});
+async function poolDemo() {
+  const pool = new Pool(credentials);
+  const now = await pool.query("SELECT NOW()");
+  await pool.end();
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-  }
-});
+  return now;
+}
 
-module.exports = sequelize;
+// Connect with a client.
+
+async function clientDemo() {
+  const client = new Client(credentials);
+  await client.connect();
+  const now = await client.query("SELECT NOW()");
+  await client.end();
+
+  return now;
+}
+
+// Use a self-calling function so we can use async / await.
+
+(async () => {
+  const poolResult = await poolDemo();
+  console.log("Time with pool: " + poolResult.rows[0]["now"]);
+
+  const clientResult = await clientDemo();
+  console.log("Time with client: " + clientResult.rows[0]["now"]);
+})();
