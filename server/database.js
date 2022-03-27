6cb4d4d72712266d5f90d72355aa8e41c0fac962
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:43:35 +0530

    ssl

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://znbfzhztgogqjp:05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dakv864hep6lie', {
+const sequelize = new Sequelize('postgres://znbfzhztgogqjp:05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dakv864hep6lie?sslmode=require', {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
