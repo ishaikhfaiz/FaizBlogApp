9f60526d411115368302b8f70c5a4e87a74ce411
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 18:51:36 +0530

    d

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'clinica-veterinaria',
-  password: 'admin',
+  user: 'znbfzhztgogqjp',
+  host: 'ec2-52-201-124-168.compute-1.amazonaws.com',
+  database: 'dakv864hep6lie',
+  password: '05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7',
   port: 5432,
 });
 
