1915f51ee1d0a22a5d2aa1582643cc4d4a6b2c42
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 19:57:49 +0530

    9

--- server/database.js
+++ server/database.js
@@ -16,13 +16,11 @@ pool.on('connect', (err, client) => {
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
-  dialect: 'postgres',
-  port: 5432,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 });
 
