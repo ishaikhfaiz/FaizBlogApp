07623db5b00e89dff3cf6920c8eeefdca7597b4d
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 15:27:10 +0530

    please

--- server/database.js
+++ server/database.js
@@ -13,20 +13,15 @@ pool.on('connect', (err, client) => {
   console.log('Successfully connected to postgres.');
 });
 
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
-  database: "dakv864hep6lie",
-  username: "znbfzhztgogqjp",
-  password: "05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7",
-  host: "ec2-52-201-124-168.compute-1.amazonaws.com",
-  port: 5432,
-  dialect: "postgres",
+const connectionString = process.env.DATABASE_URL;
+const sequelize = new Sequelize(connectionString, {
+  logging: false,
+  dialect: 'postgres',
   dialectOptions: {
-    ssl: {
-      require: true, 
-      rejectUnauthorized: false 
-    }
-  },
+    ssl: true,
+    rejectUnauthorized: false
+  }
 });
 module.exports = sequelize;
