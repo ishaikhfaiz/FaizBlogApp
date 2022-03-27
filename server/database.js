4b88269147c8a60dbd534a437720dd3c99781e0c
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:17:12 +0530

    store

--- server/database.js
+++ server/database.js
@@ -1,40 +1,28 @@
-const { Pool, Client } = require("pg");
+const { Pool } = require('pg');
 
-const credentials = {
-  user: "znbfzhztgogqjp",
-  host: "ec2-52-201-124-168.compute-1.amazonaws.com",
-  database: "dakv864hep6lie",
-  password: "05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7",
-  port: 5432,
-};
+const pool = new Pool();
 
-// Connect with a connection pool.
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
-async function poolDemo() {
-  const pool = new Pool(credentials);
-  const now = await pool.query("SELECT NOW()");
-  await pool.end();
+pool.on('connect', (err, client) => {
+  if (err) console.error(err);
+  console.log(client);
+  console.log('Successfully connected to postgres.');
+});
 
-  return now;
-}
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  }
+});
 
-// Connect with a client.
-
-async function clientDemo() {
-  const client = new Client(credentials);
-  await client.connect();
-  const now = await client.query("SELECT NOW()");
-  await client.end();
-
-  return now;
-}
-
-// Use a self-calling function so we can use async / await.
-
-(async () => {
-  const poolResult = await poolDemo();
-  console.log("Time with pool: " + poolResult.rows[0]["now"]);
-
-  const clientResult = await clientDemo();
-  console.log("Time with client: " + clientResult.rows[0]["now"]);
-})();
+module.exports = sequelize;
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/public'
   },
+  "store": {
+    "adapter": "sequelize",
+    "dialect": "postgres",
+    "url": "postgres://znbfzhztgogqjp:05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dakv864hep6lie"
+  },
   mode: 'development',
   module: {
     rules: [
