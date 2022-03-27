27906a8084a1c8eb42a6ab801706fef02d030a43
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 12:55:27 +0530

    g

--- server/database.js
+++ server/database.js
@@ -6,6 +6,7 @@ const client = new Client({
   port: '5432',
   password: '05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7',
   database: 'dakv864hep6lie'
-}) 
+});
 
 client.connect();
+client.end();
