cc94be243783eb780c418b552577e1251746d287
Faiz Shaikh <ishaikhfaiz@eng.rizvi.edu.in>
2022-03-27 15:24:02 +0530

    Great!

--- server/database.js
+++ server/database.js
@@ -15,16 +15,18 @@ pool.on('connect', (err, client) => {
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
-  dialect: 'postgres',
-  host: 'localhost',
+const sequelize = new Sequelize({
+  database: "dakv864hep6lie",
+  username: "znbfzhztgogqjp",
+  password: "05f0b48e826d0fc3c0d4c7ae63fba0cab4c84b58479e44ca789cfbdaf7ff05e7",
+  host: "ec2-52-201-124-168.compute-1.amazonaws.com",
   port: 5432,
+  dialect: "postgres",
   dialectOptions: {
-    "ssl": {
-      "require": true,
-      "rejectUnauthorized": false
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
     }
   },
 });
-
 module.exports = sequelize;
